63f615795f19a05379099e47f5bb3402a4c0a861
HX001 <hongxu2015@gmail.com>
2020-11-26 11:50:34 -0500

    Add Extra Commits

--- my-app/src/Components/App/App.js
+++ my-app/src/Components/App/App.js
@@ -10,12 +10,15 @@ function App() {
       <Container maxWidth="sm" className="App">
         <Paper>
           <img src={logo} className="App-logo" alt="logo" />
+
           <Typography variant="h4" component="h1" gutterBottom>
             Create React App + Material-UI
           </Typography>
+
           <Button variant="contained" color="primary">
             Primary Button
           </Button>
+            
           <Button variant="contained" color="secondary">
             Secondary Button
           </Button>
